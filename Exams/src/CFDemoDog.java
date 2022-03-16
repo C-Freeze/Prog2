187f8fd909f5f89315f4ab212b530710275c2ded
asdf <asdf>
2022-03-16 11:27:22 -0500

    asdfasdfasdf

---
+++ Exams/src/CFDemoDog.java
@@ -0,0 +1,14 @@
+// CFDemoDog.java
+// Carter Freeze
+// 3/16/22
+// Exam 2 Part 2
+public class CFDemoDog {
+    public static void main(String[] args) {
+        CFDog dog1 = new CFDog("Fido", "Mutt", 35);
+        CFDog dog2 = new CFDog("Bingo");
+
+        System.out.println(dog1);
+        System.out.println(dog2);
+    }
+    
+}
